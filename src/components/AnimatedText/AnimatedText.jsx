d56545e0d16e58c818d1a027a9a46ce63ee989b4
Víctor Quiñones <victhorq716@gmail.com>
2025-01-08 15:03:59 -0400

    Refactor AnimatedText component: restructure file organization, enhance typewriter effect, and update styles

---
+++ src/components/AnimatedText/AnimatedText.jsx
@@ -0,0 +1,42 @@
+import Typewriter from 'typewriter-effect'
+import './style.css'
+
+export const AnimatedText = () => {
+  const typeText =
+    'font-mono text-[1rem] sm:text-xl md:text-2xl leading-loose text-gray-200 p-2'
+
+  return (
+    <div className='flex items-center justify-center text-center aspect-auto'>
+      <div className={typeText}>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .pauseFor(300)
+              .typeString(
+                '<span>console.log("<span class="string">¡Hola Mundo!</span>")</span>'
+              )
+              .pauseFor(300)
+              .deleteAll('natural')
+              .typeString(
+                '<span>print("<span class="string">Hello World!</span>")</span>'
+              )
+              .deleteAll('natural')
+              .typeString(
+                '<span>puts "<span class="string">Ciao Mondo!</span>"</span>'
+              )
+              .deleteAll('natural')
+              .typeString(
+                '<span>echo "<span class="string">こんにちは世界</span>"</span>'
+              )
+              .pauseFor(300)
+              .start()
+          }}
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
+    </div>
+  )
+}
