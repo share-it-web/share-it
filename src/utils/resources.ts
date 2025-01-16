bc3916fcc60785b13e3f262242b77c6945893bfb
Víctor Quiñones <victhorq716@gmail.com>
2025-01-16 15:53:12 -0400

    add more resourses

--- src/utils/resources.ts
+++ src/utils/resources.ts
@@ -8,6 +8,22 @@ interface Resource {
 
 export const resources: Resource[] = [
   {
+    title: 'Java',
+    description:
+      'Guías, tutoriales, libros y todo recurso para aprender y mejorar con Java',
+    slug: 'https://github.com/eliasvelazquezdev/share-it-resources/tree/main/lenguajes/java',
+    date: new Date('2025-01-14'),
+    category: 'Java',
+  },
+  {
+    title: 'CSS',
+    description:
+      'Guías, tutoriales, libros y todo recurso para aprender y mejorar en CSS.',
+    slug: 'https://github.com/eliasvelazquezdev/share-it-resources/tree/main/lenguajes/css',
+    date: new Date('2025-01-15'),
+    category: 'CSS',
+  },
+  {
     title: 'JavaScript',
     description:
       'Descubre proyectos, tutoriales y recursos para aprender JavaScript.',
